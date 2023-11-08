487726a176376dcea1d535f816f391991110adec
Salma Ahmed Mostafa <56895305+salma234-del@users.noreply.github.com>
2023-11-08 18:27:51 +0200

    Time: 4 ms (29.27%), Space: 6.2 MB (10.74%) - LeetHub

--- 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -3,8 +3,6 @@ public:
     bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
         if(sx == fx && sy == fy && t == 1) return false;
         int dis = max(abs(sx - fx) , abs(sy - fy));
-        cout << dis << '\n';
         return dis <= t;
-      
     }
 };
