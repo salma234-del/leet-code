dc75c216d410428b565d00b346c80147d58f351a
Salma Ahmed Mostafa <56895305+salma234-del@users.noreply.github.com>
2024-07-28 21:35:57 +0300

    Time: 0 ms (100.00%), Space: 7.5 MB (28.02%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int c = 0, res = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == 'L') c++;
+            else c--;
+            
+            if(c == 0) res++;
+        }
+        return res;
+    }
+};
